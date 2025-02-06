c4df2872ec39e063f28be687f290fb14809b440a
annite01 <anhphong911976@gmail.com>
2025-02-06 17:30:45 +0700

    Add files via upload

--- Vesion1.2.lua
+++ Vesion1.2.lua
@@ -24,7 +24,7 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
        Image = "http://www.roblox.com/asset/?id=16513855098",
        Size = {41, 40},
        Color = Color3.fromRGB(10, 15, 15),
-       Corner = false,
+       Corner = true,
        Stroke = true,
        StrokeColor = Color3.fromRGB(255, 250, 255)
       })
@@ -38,8 +38,105 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/daucobonhi/Ui-Redz-V
      local Tab8o = MakeTab({Name = "Sea Even"})
      local Tab9o = MakeTab({Name = "Misc"})
      local Tab2o = MakeTab({Name = "Settings"})
-     
-     
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+    repeat wait() until game.Players
+    repeat wait() until game.Players.LocalPlayer
+    repeat wait() until game.ReplicatedStorage
+    repeat wait() until game.ReplicatedStorage:FindFirstChild("Remotes");
+    repeat wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui");
+    repeat wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
+    repeat wait() until game:GetService("Players")
+    repeat wait() until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Energy")
+    
+    wait(0.1)
+    
+    if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
+    
+    if game:GetService("Players").LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam") then
+        repeat wait()
+            if game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Main").ChooseTeam.Visible == true then
+                if _G.Team == "Pirate" then
+                    for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton.Activated)) do                                                                                                
+                        v.Function()
+                    end
+                elseif _G.Team == "Marine" then
+                    for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Marines.Frame.ViewportFrame.TextButton.Activated)) do                                                                                                
+                        v.Function()
+                    end
+                else
+                    for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container.Pirates.Frame.ViewportFrame.TextButton.Activated)) do                                                                                                
+                        v.Function()
+                    end
+                end
+            end
+        until game.Players.LocalPlayer.Team ~= nil and game:IsLoaded()
+    end
+	
+
+--// Select Island
+if First_Sea then
+AreaList = {
+  'Jungle', 'Buggy', 'Desert', 'Snow', 'Marine', 'Sky', 'Prison', 'Colosseum', 'Magma', 'Fishman', 'Sky Island', 'Fountain'
+} elseif Second_Sea then
+AreaList = {
+  'Area 1', 'Area 2', 'Zombie', 'Marine', 'Snow Mountain', 'Ice fire', 'Ship', 'Frost', 'Forgotten'
+} elseif Third_Sea then
+AreaList = {
+  'Pirate Port', 'Amazon', 'Marine Tree', 'Deep Forest', 'Haunted Castle', 'Nut Island', 'Ice Cream Island', 'Cake Island', 'Choco Island', 'Candy Island','Tiki Outpost'
+}
+end
+
+        --// Tween Island
+        function TP2(P1)
+        local Distance = (P1.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
+        if Distance >= 1 then
+        Speed = 350
+        end
+        game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(Distance/Speed, Enum.EasingStyle.Linear), {
+          CFrame = P1
+        }):Play()
+        if _G.CancelTween2 then
+        game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(Distance/Speed, Enum.EasingStyle.Linear), {
+          CFrame = P1
+        }):Cancel()
+        end
+        _G.Clip2 = true
+        wait(Distance/Speed)
+        _G.Clip2 = false
+        end
+
+
+
+ function Tween(Pos)
+        Distance = (Pos.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
+        if game.Players.LocalPlayer.Character.Humanoid.Sit == true then game.Players.LocalPlayer.Character.Humanoid.Sit = true end
+        pcall(function() tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(Distance/350, Enum.EasingStyle.Linear),{CFrame = Pos}) end)
+        tween:Play()
+        if Distance <= 300 then
+            tween:Cancel()
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Pos
+        end
+        if _G.StopTween == true then
+            tween:Cancel()
+            _G.Clip = false
+        end
+    end
+    
+------// BLOX FRUIT
+--// Sea world
+First_Sea = false
+Second_Sea = false
+Third_Sea = false
+local placeId = game.PlaceId
+if placeId == 2753915549 then
+First_Sea = true
+elseif placeId == 4442272183 then
+Second_Sea = true
+elseif placeId == 7449423635 then
+Third_Sea = true
+end
+
 ----- Dropdown 
 
   Dropdown = AddDropdown(Tab1o, {
@@ -76,7 +173,7 @@ Toggle = AddToggle(Tab1o, {
 Section = AddSection(Tab1o, {"Farm Level"})      
   Dropdown = AddDropdown(Tab1o, {
      Name = "Select Mod Farm",
-     Options = {"Level", "Bone", "Katakuri", "Mod Aura"},
+     Options = {"Level [Config Bugs]", "Bone", "Katakuri", "Mod Aura"},
      Default = "Select Farm",
      Callback = function()
      end
@@ -87,9 +184,8 @@ Toggle = Add
       Name = "Farm",
       Default = false,
       Callback = function()
-     end
-    })
-    
+  end
+  })
 
     Section = AddSection(Tab1o, {"FPS"})      
     AddButton(Tab1o, {
@@ -143,7 +239,7 @@ Dropdown = AddDropdown(Tab2o, {
     
 
      Toggle = AddToggle(Tab2o, {
-      Name = "Anti Lag",
+      Name = "Fast Attach [Beta]",
       Default = true,
       Callback = function()
      end
@@ -154,6 +250,25 @@ Dropdown = AddDropdown(Tab2o, {
       Name = "Anti Afk",
       Default = true,
       Callback = function()
+     end
+    })
+    
+
+     Toggle = AddToggle(Tab2o, {
+      Name = "Remove Dame",
+      Default = true,
+      Callback = function()
+    spawn(function()
+        while wait() do
+            if FaiFaoRemovetext then
+                game:GetService("ReplicatedStorage").Assets.GUI.DamageCounter.Enabled = false
+            else
+                game:GetService("ReplicatedStorage").Assets.GUI.DamageCounter.Enabled = true
+            end
+        end
+        end)
+
+
      end
     })
     
@@ -233,8 +348,7 @@ Toggle = AddToggle(Tab2o, {
      end
     })
 -----Tele
-    Section = AddSection(Tab4o, {"Soon..."})      
-    Section = AddSection(Tab4o, {"Update config"})      
+    Section = AddSection(Tab4o, {"Soon!!"})      
 -----Raid
     Section = AddSection(Tab5o, {"Raid"})      
     Dropdown = AddDropdown(Tab5o, {
@@ -261,6 +375,32 @@ Toggle = AddToggle(Tab5o, {
     })
 
 Toggle = AddToggle(Tab5o, {
+      Name = "Kill Aura",
+      Default = false,
+      Callback = function()
+spawn(function()
+    while wait() do
+        if KillAura then
+            pcall(function()
+                for i,v in pairs(game.Workspace.Enemies:GetDescendants()) do
+                    if v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health > 0 then
+                        repeat task.wait()
+                            sethiddenproperty(game:GetService('Players').LocalPlayer,"SimulationRadius",math.huge)
+                            v.Humanoid.Health = 0
+                            v.HumanoidRootPart.CanCollide = false
+                        until not KillAura or not v.Parent or v.Humanoid.Health <= 0
+                    end
+                end
+            end)
+        end
+    end
+end)
+
+
+     end
+    })
+
+Toggle = AddToggle(Tab5o, {
       Name = "Next Island",
       Default = false,
       Callback = function()
@@ -274,6 +414,24 @@ Toggle = AddToggle(Tab5o, {
      end
     })
 -----Shop
+    Section = AddSection(Tab6o, {"Fruits"})      
+Toggle = AddToggle(Tab6o, {
+      Name = "Aotu Random Fruits",
+      Default = false,
+      Callback = function()
+    _G.Random_Auto = Value
+spawn(function()
+    pcall(function()
+        while wait(.1) do
+            if _G.Random_Auto then
+                game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("Cousin","Buy")
+            end 
+        end
+    end)
+end)
+     end
+    })
+
     Section = AddSection(Tab6o, {"Haki"})      
     AddButton(Tab6o, {
      Name = "Geppo",
